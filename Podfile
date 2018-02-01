42f557bbf6aea13f9bf4c3aef1edab345e49aec4
sudashui <sudashui@github.com>
2018-02-01 17:35:40 +0800

    项目创建
    环境搭建

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'git@github.com:CocoaPods/Specs.git'
+source 'https://github.com/cocoaPods/specs.git'
+platform:ios,'7.0'
+target 'WeekDemo' do
+pod 'WeexSDK'
+pod 'SocketRocket'
+pod 'SDWebImage'
+pod 'WXDevtool'
+end
---
+++ WeekDemo/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  WeekDemo
+//
+//  Created by Majingqi on 2018/2/1.
+//  Copyright © 2018年 com.bl. All rights reserved.
+//
+
+#import "ViewController.h"
+//#import <weexSDK/WXSDKInstance.h>
+@interface ViewController ()
+
+//@property (nonatomic, strong) WXSDKInstance *instance;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+//- (WXSDKInstance *)instance
+//{
+//    if (_instance == nil) {
+//        _instance = [[WXSDKInstance alloc] init];
+//    }
+//    return _instance;
+//}
+
+
+@end
